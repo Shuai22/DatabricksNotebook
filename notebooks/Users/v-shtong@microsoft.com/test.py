91026bf3119244881a06580944ae50b80bd76026
Shuai <42922299+Shuai22@users.noreply.github.com>
2020-09-21 13:48:24 +0800

    Update: README.md notebooks

---
+++ notebooks/Users/v-shtong@microsoft.com/test.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession
+from pyspark.sql.types import * 
+
+spark.conf.set("fs.azure.account.auth.type.azuresynapsepiero.dfs.core.windows.net", "SharedKey")
+spark.conf.set("fs.azure.account.key.azuresynapsepiero.dfs.core.windows.net","JzKIpiBzW7WQQhcx0zNTtq52DlSmniO0RNKMwG0olbMYE4t1AUsYebRNWj5ZHGYnva/jRuCGXoN10cwhwBeBuQ==")
+
+# COMMAND ----------
+
+yellow_path = "abfss://root@azuresynapsepiero.dfs.core.windows.net/nyctlc/yellow/*/*/"
+
+# COMMAND ----------
+
+df_fhv = spark.read.parquet(Fhv_path)
+
+
+# COMMAND ----------
+
+df_fhv.show(1)
